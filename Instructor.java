25f36b60ad7c84b33203e55a689d85c5607dcc30
Mubarak <m.u.b2011@hotmail.com>
2021-11-18 22:27:13 +0400

    Created 3 extra classes

---
+++ Instructor.java
@@ -0,0 +1,16 @@
+
+public class Instructor {
+	private String firstName;
+	private String lastName;
+	public Instructor(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return this.firstName;
+	}
+	public String getLastName() {
+		return this.lastName;
+	}
+
+}
